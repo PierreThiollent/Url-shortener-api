d1e49f97cc4834adf7a0b2b4068ae103b8154b34
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-03-11 20:00:37 +0100

    feat(): Add middleware to manage CORS, an other to serve public folder and serve index.html on website home

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require('express');
+var cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+
+// On sert le dossier public de manière statique
+app.use('/public', express.static(process.cwd() + '/public'));
+
+// On retourne l'index.html sur la route /
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 module.exports = app;
